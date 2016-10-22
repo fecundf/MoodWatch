e758b3567635af5a21e581c9806c4f4065211340
fecundf <not.com+github@gmail.com>
2016-10-22 19:08:03 -0400

    old changes can be distcarded

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 var express = require('express');
-var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // create new FitbitApiClient object
   var FitbitApiClient = require('fitbit-node');
   // authenticate app to use Fitbit API, client is a global declared in app.js
-  client = new FitbitApiClient("2282X4", "4425ec1dbcd9f8a60a884ee8288a5acb");
+  client = new FitbitApiClient("2282XJ", "9b95c0621903b3bcfa7f63ecda863a5f");
   res.redirect(client.getAuthorizeUrl(
     'activity heartrate location profile settings sleep weight',
     'https://api.scriptrapps.io/MoodWatch.js'));
 });
-
-module.exports = router;
 
 
