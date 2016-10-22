3a9067040d6425cedcc91b0bfb83884679261b96
Jan10e <a.c.broek@gmail.com>
2016-10-22 18:33:07 -0400

    a lot of stuff, sorry

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 var express = require('express');
-var router = express.Router();
+var app = express();
+//var heartbeat = require()
+
+app.set('port', (process.env.PORT || 5000));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res) {
   // create new FitbitApiClient object
   var FitbitApiClient = require('fitbit-node');
   // authenticate app to use Fitbit API, client is a global declared in app.js
-  client = new FitbitApiClient("2282X4", "4425ec1dbcd9f8a60a884ee8288a5acb");
+  client = new FitbitApiClient("2282XJ", "9b95c0621903b3bcfa7f63ecda863a5f");
   res.redirect(client.getAuthorizeUrl(
     'activity heartrate location profile settings sleep weight',
     'https://api.scriptrapps.io/MoodWatch.js'));
 });
 
-module.exports = router;
+// GET heartbeat info
+/*app.get('/heartbeat', funtion(request, response) {
+  response.send(heartbeat());
+});*/
 
-
-express.listen(express.get('port'), function() {
+app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
