71aa7821b4ba1b9f0a94bf84a97078de69a61dfb
Jan10e <a.c.broek@gmail.com>
2016-10-22 18:43:00 -0400

    add callback

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-//var heartbeat = require()
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,13 +11,24 @@ app.get('/', function(req, res) {
   client = new FitbitApiClient("2282XJ", "9b95c0621903b3bcfa7f63ecda863a5f");
   res.redirect(client.getAuthorizeUrl(
     'activity heartrate location profile settings sleep weight',
-    'https://api.scriptrapps.io/MoodWatch.js'));
+    'https://secure-river-97770.herokuapp.com/callback'));
 });
 
-// GET heartbeat info
-/*app.get('/heartbeat', funtion(request, response) {
-  response.send(heartbeat());
-});*/
+// handle the callback from the Fitbit authorization flow
+app.get("/callback", function (req, res) {
+    // exchange the authorization code we just received for an access token
+    client.getAccessToken(req.query.code, 'https://secure-river-97770.herokuapp.com/callback').then(function (result) {
+        // use the access token to fetch the user's profile information
+        client.get("/profile.json", result.access_token).then(function (results) {
+            res.send(results[0]);
+        });
+    }).catch(function (error) {
+        res.send(error);
+    });
+});
+
+// launch the server
+//app.listen(3000);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
